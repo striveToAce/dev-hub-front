ad1917f0a0e4457c0e56361fceece013797d7e2e
Ashish Kumar <ashish380431@gmail.com>
2024-03-18 13:04:03 +0530

    feeds layout fixes

--- src/app/feeds/page.tsx
+++ src/app/feeds/page.tsx
@@ -2,7 +2,7 @@ import FeedCard from "../components/feeds/FeedCard";
 
 const Feeds = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-2 m-2">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 m-2">
       <FeedCard />
       <FeedCard />
       <FeedCard />
