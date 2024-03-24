1b0e8bb836a2abe52b832c81ebebd9113c0ed09f
Ashish Kumar <ashish380431@gmail.com>
2024-03-24 13:10:19 +0530

    add feed api integration with UI and formik validation and submission

---
+++ src/app/api/feed/list/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+import { getUser } from "../../middleware";
+
+const responseHandler = (status: any, data: any, code: any, message: any) => {
+  return NextResponse.json({
+    status,
+    data,
+    code,
+    message,
+  });
+};
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { page, pagesize } = body;
+    const startIndex = (page - 1) * pagesize;
+    const endIndex = startIndex + pagesize - 1;
+
+    const supabase = createClient();
+    const { data, error } = await supabase
+      .from("feeds")
+      .select("*")
+      .range(startIndex, endIndex);
+
+    if (error) return responseHandler(0, {}, 500, "something went wrong!");
+    return responseHandler(1, data, 200, "Data fetched successfully");
+  } catch (err) {
+    return responseHandler(0, {}, 500, "something went wrong!");
+  }
+}
